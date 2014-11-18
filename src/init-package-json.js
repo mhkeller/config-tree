93b65bc5b100ac3f8687eaadd857658fc86f3190
mhkeller <michael.keller@gmail.com>
2014-11-18 10:09:04 -0500

    extend sample config

--- src/init-package-json.js
+++ src/init-package-json.js
@@ -1,13 +1,14 @@
-
 module.exports = init
-var PZ = require('promzard').PromZard
-var path = require('path')
+var PZ = require('promzard').PromZard;
+var path = require('path');
 
-var fs = require('fs')
-var read = require('read')
+var fs = require('fs');
+var read = require('read');
+
+var _ = require('underscore');
 
 function dirExists(path){
-  return fs.existsSync(path)
+  return fs.existsSync(path);
 }
 
 function init (dir, name_prefix, input, config, cb) {
@@ -33,7 +34,9 @@ function init (dir, name_prefix, input, config, cb) {
   		conf_dir_exists = dirExists( conf_dir_path );
 
   // If it doesn't exist, create the `.conf` folder
-  if (!conf_dir_exists)	fs.mkdirSync( conf_dir_path );
+  if (!conf_dir_exists)	{
+  	fs.mkdirSync( conf_dir_path );
+  }
 
   var package,
       pkg,
@@ -43,40 +46,40 @@ function init (dir, name_prefix, input, config, cb) {
       config_exists = dirExists( config_path );
 
   if (config_exists){
-    package = config_path
-    pkg = require(config_path)
+    package = config_path;
+    pkg = _.extend(require(config_sample_path), require(config_path)); // extend the sample config so it will grab any new keys if you made additions the sample schema
   } else {
-    package = config_sample_path
-    pkg = require(config_sample_path)
+    package = config_sample_path;
+    pkg = require(config_sample_path);
   }
 
-  package_out = config_path // Possibly change this to ~/.conf/ but that would require program-specific config file names and things
+  package_out = config_path;
 
-  input = path.resolve(input)
-  var ctx = {}
-  ctx.package = pkg
+  input = path.resolve(input);
+  var ctx = {};
+  ctx.package = pkg;
 
   // make sure that the input is valid.
   // if not, use the default
-  var pz = new PZ(input, ctx)
+  var pz = new PZ(input, ctx);
 
-  var def = require.resolve(dir + '/default-input.js')
-  pz.backupFile = def
-  pz.on('error', cb)
+  var def = require.resolve(dir + '/default-input.js');
+  pz.backupFile = def;
+  pz.on('error', cb);
   pz.on('data', function (data) {
 
     Object.keys(data).forEach(function (k) {
-      if (data[k] !== undefined && data[k] !== null) pkg[k] = data[k]
+      if (data[k] !== undefined && data[k] !== null) pkg[k] = data[k];
     })
 
-    var d = JSON.stringify(pkg, null, 2) + '\n'
-    console.log('About to write to %s:\n\n%s\n', package_out, d)
+    var d = JSON.stringify(pkg, null, 2) + '\n';
+    console.log('About to write to %s:\n\n%s\n', package_out, d);
     read({prompt:'Is this ok? ', default: 'yes'}, function (er, ok) {
       if (!ok || ok.toLowerCase().charAt(0) !== 'y') {
-        console.log('Aborted.')
+        console.log('Aborted.');
       } else {
         fs.writeFile(package_out, d, 'utf8', function (er) {
-          return cb(er, pkg)
+          return cb(er, pkg);
         })
       }
     })
